0d0ed133405c09b6b69f311757fd8e516770dadf
Mohamed Nazeer <st10293093@vcconnect.edu.za>
2025-11-21 16:12:04 +0200

    feat: Implement initial message routes and controllers, and configure frontend with Tailwind CSS.

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,17 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import {
+  getUsersForSidebar,
+  getMessages,
+  sendMessage,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+// Gets the id of the user for message history
+router.get("/:id", protectRoute, getMessages);
+// Send messages
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
